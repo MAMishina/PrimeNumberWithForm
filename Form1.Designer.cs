e76f9969fcc29c30d600b9c6b2832732a80866d7
Мария Мишина <mishinamariyaandreevna@gmail.com>
2022-09-18 23:06:48 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace PrimeNumberWithForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.check_button1 = new System.Windows.Forms.Button();
+            this.input_textBox1 = new System.Windows.Forms.TextBox();
+            this.answer_label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // check_button1
+            // 
+            this.check_button1.Location = new System.Drawing.Point(12, 58);
+            this.check_button1.Name = "check_button1";
+            this.check_button1.Size = new System.Drawing.Size(289, 29);
+            this.check_button1.TabIndex = 0;
+            this.check_button1.Text = "Тест Миллера Раббина";
+            this.check_button1.UseVisualStyleBackColor = true;
+            this.check_button1.Click += new System.EventHandler(this.check_button1_Click);
+            // 
+            // input_textBox1
+            // 
+            this.input_textBox1.Location = new System.Drawing.Point(12, 12);
+            this.input_textBox1.Name = "input_textBox1";
+            this.input_textBox1.Size = new System.Drawing.Size(289, 27);
+            this.input_textBox1.TabIndex = 1;
+            this.input_textBox1.UseWaitCursor = true;
+            this.input_textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.input_textBox1_KeyPress);
+            //this.input_textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.input_textBox1_KeyUp);
+            // 
+            // answer_label1
+            // 
+            this.answer_label1.AutoSize = true;
+            this.answer_label1.Location = new System.Drawing.Point(12, 125);
+            this.answer_label1.Name = "answer_label1";
+            this.answer_label1.Size = new System.Drawing.Size(289, 20);
+            this.answer_label1.TabIndex = 2;
+            this.answer_label1.Text = "Является ли введенное число простым?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(289, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Простой перебор делителей";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 206);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.answer_label1);
+            this.Controls.Add(this.input_textBox1);
+            this.Controls.Add(this.check_button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button check_button1;
+        private TextBox input_textBox1;
+        private Label answer_label1;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace PrimeNumberWithForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void check_button1_Click(object sender, EventArgs e)
+        {
+            DateTime start = DateTime.Now;
+            answer_label1.Text = "Является ли введенное число простым?";
+            BigInteger number;
+            bool tryParse = BigInteger.TryParse(input_textBox1.Text, out number);
+            if (tryParse)
+            {
+                if (MillerRabinTest(number, 1000))
+                {
+                    answer_label1.Text += "\nДа!";
+                }
+                else
+                {
+                    answer_label1.Text += "\nНет!";
+                }
+            }
+            else
+            {
+                answer_label1.Text += "\nВы осуществили некорректный ввод.\nПопробуйте еще раз.";
+            }
+            TimeSpan timeOfWork = DateTime.Now - start;
+            answer_label1.Text += "\nВремя выполнения программы: " + timeOfWork.ToString();
+        }
+
+        static bool MillerRabinTest(BigInteger n, int k)
+        {
+            
+            // если n == 2 или n == 3 - эти числа простые, возвращаем true
+            if (n == 2 || n == 3)
+                return true;
+
+            // если n < 2 или n четное - возвращаем false
+            if (n < 2 || n % 2 == 0)
+                return false;
+
+            // представим n − 1 в виде (2^s)·t, где t нечётно, это можно сделать последовательным делением n - 1 на 2
+            BigInteger t = n - 1;
+
+            int s = 0;
+
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s += 1;
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+                byte[] _a = new byte[n.ToByteArray().LongLength];
+
+                BigInteger a;
+
+                do
+                {
+                    rng.GetBytes(_a);
+                    a = new BigInteger(_a);
+                }
+                while (a < 2 || a >= n - 2);
+                BigInteger x = BigInteger.ModPow(a, t, n);
+                if (x == 1 || x == n - 1)
+                    continue;
+
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, n);
+                    if (x == 1)
+                        return false;
+                    if (x == n - 1)
+                        break;
+                }
+
+                if (x != n - 1)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            answer_label1.Text = "Является ли введенное число простым?\n";
+            BigInteger number;
+            bool tryParse = BigInteger.TryParse(input_textBox1.Text, out number);
+            int countDel = 1;
+            if (tryParse)
+            {
+                DateTime start = DateTime.Now;
+                for (BigInteger i = 1; i <= (number/2 + 1); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        countDel++;
+                        answer_label1.Text += " "+ i;
+                        if (countDel > 2)
+                        {
+                            answer_label1.Text += "\nНет!";
+                            break;
+                        }
+                    }
+                    
+                }
+                
+                TimeSpan finish = DateTime.Now - start;
+                answer_label1.Text+= "\nВремя выполнения программы: " + finish.ToString();
+            }
+        }
+
+        private void input_textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && !(e.KeyChar == 8))
+            {
+                e.Handled = true;
+            }
+        }
+
+    }
+}
